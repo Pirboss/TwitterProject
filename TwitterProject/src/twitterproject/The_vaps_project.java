e55cf18b419e55866dadd522de9930297457b155
ValentinGenin <genin.valentin@gmail.com>
2017-01-26 11:19:14 +0100

    Test début Pierre

---
+++ TwitterProject/src/twitterproject/The_vaps_project.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package twitterproject;
+
+import java.util.List;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+import twitter4j.*;
+
+/**
+ *
+ * @author pierre
+ */
+public class The_vaps_project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws TwitterException {
+        // TODO code application logic here
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("gVaO166IGfSCun5Esj1sZ5bX3")
+                .setOAuthConsumerSecret("KhN1L4pXCG5U6d6tee1IGUnIV1bqxieQJDxE2HOx3AGI5npQRE")
+                .setOAuthAccessToken("822018210178469888-4Pr1KRE2rmFvof0O5IUdTlxtkGKamU4")
+                .setOAuthAccessTokenSecret("D1thDl7k8v2s1SupU5ZGhK3U7Mphb69fpUW4JuDou5O65");
+        
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        twitter4j.Twitter twitter = tf.getInstance();
+        
+        List<Status> status = twitter.getHomeTimeline();
+        for(Status st : status){
+            System.out.println(st.getUser().getName()+"--------"+st.getText());
+        }
+        
+        
+        // The factory instance is re-useable and thread safe.
+        Twitter twitter2 = tf.getSingleton();
+        Query query = new Query("source:twitter4j yusukey");
+        QueryResult result = twitter.search(query);
+        for (Status statuss : result.getTweets()) {
+            System.out.println("@" + statuss.getUser().getScreenName() + ":" + statuss.getText());
+        }
+        
+        
+                
+        
+    }
+    
+}
