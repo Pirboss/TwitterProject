e9fe9db1d30da2f5e13f3df987a91b411775a843
admin <admin@DESKTOP-C66F7E9.home>
2017-03-17 08:43:56 +0100

    Regroupement des tweets par topic.

---
+++ src/the_vaps_project/analysis/TweetsPerTopic.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package the_vaps_project.analysis;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.openide.util.Exceptions;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+/**
+ *
+ * @author admin
+ */
+public class TweetsPerTopic {
+    private Document doc;
+    Element rootElement;
+    
+    public TweetsPerTopic() {
+        try {
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+            // root elements
+            doc = docBuilder.newDocument();
+            rootElement = doc.createElement("topics");
+            doc.appendChild(rootElement);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void addTopic(String topicName) {
+        NodeList topics = rootElement.getElementsByTagName("topic");
+        Boolean isAlreadyCreated = false;
+        
+        for(int i=0; i<topics.getLength(); i++){
+            isAlreadyCreated = topics.item(i).getAttributes().getNamedItem("name").getNodeValue().equals(topicName);
+        }
+        
+        if (!isAlreadyCreated) {
+            Element topic = doc.createElement("topic");
+            topic.setAttribute("name", topicName);
+            rootElement.appendChild(topic);
+        }
+    }
+    
+    public void addTweet(String topicName, String tweetId) {
+        Element tweet = doc.createElement("tweet");
+        NodeList topics = rootElement.getElementsByTagName("topic");
+        
+        for(int i=0; i<topics.getLength(); i++){
+            if (topics.item(i).getAttributes().getNamedItem("name").getNodeValue().equals(topicName)) {
+                tweet.setAttribute("id", tweetId);
+                topics.item(i).appendChild(tweet);
+            }
+        }
+    }
+    
+    public void createFile() {
+        try {
+            // write the content into xml file
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new FileOutputStream("tweetsPerTopic.xml"));
+            
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+            
+            transformer.transform(source, result);
+        } catch (TransformerException ex) {
+            Exceptions.printStackTrace(ex);
+        } catch (FileNotFoundException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+}
+    
+}
---
+++ src/the_vaps_project/analysis/TweetsReader.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package the_vaps_project.analysis;
+
+import java.util.List;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ *
+ * @author admin
+ */
+public class TweetsReader {
+    
+    private static TweetsPerTopic tpt = new TweetsPerTopic();
+    
+    public static Boolean topicMatch(String text, List<String> topicWords) {
+        Boolean res = false;
+        for (String word : topicWords) {
+            res = res || (text.contains(word));
+        }
+        return res;
+    }
+    
+    public static void main (String args[]) {
+        
+        try {
+            
+            
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();            
+            DefaultHandler handler = new DefaultHandler() {
+                boolean bTweet = false;
+                boolean bId = false;
+                boolean bText = false;
+                String id = "";
+                String text = "";
+                List<Topic> topics = new TopicsGenerator().calculTopicsIdeal();
+                        
+                public void startElement(String uri, String localName,String qName, Attributes attributes) throws SAXException {
+                    if(qName.equalsIgnoreCase("tweet")) {
+                        bTweet = true;
+                    }
+                    if(qName.equalsIgnoreCase("id")) {
+                        bId = true;
+                        id = "";
+                    }
+                    if(qName.equalsIgnoreCase("text")) {
+                        bText = true;
+                        text = "";
+                    }
+                }
+                
+                public void characters(char ch[], int start, int length) throws SAXException {
+                    if (bId) {
+                        id = new String(ch, start, length);
+                    }
+                    if (bText) {
+                        text = new String(ch, start, length);
+                    }
+                }
+                
+                public void endElement(String uri, String localName,String qName) throws SAXException {
+                    if (bTweet) {
+                        bTweet = false;
+                        
+                        // comparaison avec les thèmes trouvés
+                        for(Topic t : topics) {
+                            if (topicMatch(text, t.getWords())) {
+                                //associer l'id au thème
+                                tpt.addTopic(t.getName());
+                                tpt.addTweet(t.getName(), id);
+                            }
+                        }
+                    }
+                    if (bId) {
+                        bId = false;
+                    }
+                    if (bText) {
+                        bText = false;
+                    }
+                }
+
+            };
+            
+            saxParser.parse("tweets.xml", handler);
+            tpt.createFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
